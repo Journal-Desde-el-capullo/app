2b0f3bc0d0335ad7bd9ecb246e4e2652f1967728
Dianahincapiea87 <dianahincapiea87@gmail.com>
2026-04-22 13:43:07 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,13 @@
-const CACHE = "capullo-v14";
-self.addEventListener("install", e => { self.skipWaiting(); });
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+const CACHE="capullo-v12";
+const ASSETS=["/app/","/app/index.html","/app/manifest.json"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(k=>Promise.all(k.map(n=>n!==CACHE?caches.delete(n):null))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
